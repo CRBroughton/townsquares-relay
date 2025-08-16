c925b774425ac2d49a312712930f025bc74818fa
CRBroughton <crbroughton@posteo.uk>
2025-08-16 18:04:24 +0100

    feat(relay): :sparkles: add external configuration file relay

--- main.go
+++ main.go
@@ -2,19 +2,47 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/fiatjaf/khatru"
 	"github.com/nbd-wtf/go-nostr"
 )
 
-func main() {
-	relay := khatru.NewRelay()
+type Config struct {
+	Port        string `json:"port"`
+	Name        string `json:"name"`
+	PubKey      string `json:"pubkey"`
+	Description string `json:"description"`
+}
 
-	relay.Info.Name = "Townsquares relay"
-	relay.Info.PubKey = "PUB_KEY_HERE"
-	relay.Info.Description = "A townsquares relay"
+func loadConfig(filename string) (*Config, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read config file: %w", err)
+	}
+
+	var config Config
+	if err := json.Unmarshal(data, &config); err != nil {
+		return nil, fmt.Errorf("failed to parse config file: %w", err)
+	}
+
+	return &config, nil
+}
+
+func main() {
+	config, err := loadConfig("config.json")
+	if err != nil {
+		log.Fatalf("Error loading config: %v", err)
+	}
+
+	relay := khatru.NewRelay()
+	relay.Info.Name = config.Name
+	relay.Info.PubKey = config.PubKey
+	relay.Info.Description = config.Description
 
 	// TODO - add actual storage
 	store := make(map[string]*nostr.Event, 120)
@@ -42,6 +70,6 @@ func main() {
 		w.Header().Set("content-type", "text/html")
 	})
 
-	fmt.Println("running on :3334")
-	http.ListenAndServe(":3334", relay)
+	fmt.Printf("running on %s\n", config.Port)
+	log.Fatal(http.ListenAndServe(config.Port, relay))
 }
